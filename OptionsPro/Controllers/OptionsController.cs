aa589e2ae1e4feb3635c2093d08002e2ec611671
Edward <v-tazho@hotmail.com>
2018-08-27 14:30:29 +0800

    options

---
+++ OptionsPro/Controllers/OptionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using OptionsPro.Models;
+
+namespace OptionsPro.Controllers
+{
+    public class OptionsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly Locations _locations;
+        public OptionsController(IConfiguration configuration
+            , IOptions<Locations> options)
+        {
+            _configuration = configuration;
+            _locations = options.Value;
+        }
+        public IActionResult Index()
+        {
+            var locations = new Locations();
+            _configuration.GetSection("Locations").Bind(locations);
+
+            var items = locations.location.AsEnumerable();
+            var items2 = _locations;
+            return View();
+        }
+    }
+}
---
+++ OptionsPro/Models/Locations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsPro.Models
+{
+    public class Locations
+    {
+        public List<string> location { get; set; }
+    }
+}
