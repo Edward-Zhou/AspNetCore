f8d0c911a4218323fe1f20e08104f89bc41fefe7
Edward <v-tazho@hotmail.com>
2018-09-27 13:23:10 +0800

    file upload

---
+++ MVCPro/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost("images")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            return Ok("Work");
+        }
+    }
+}
--- MVCPro/Startup.cs
+++ MVCPro/Startup.cs
@@ -36,8 +36,8 @@ namespace MVCPro
             services.AddTransient((serviceProvider)=> new Claim { Type = "T1", Value = "V1" });
             services.AddMvc(c =>
                             {
-                                c.Filters.Add(typeof(RequestLoggerActionFilter));
-                                c.Filters.Add(typeof(ClaimRequirementFilter));
+                                //c.Filters.Add(typeof(RequestLoggerActionFilter));
+                                //c.Filters.Add(typeof(ClaimRequirementFilter));
                                 //c.Filters.Add(new ClaimRequirementFilter(new Claim { Type = "T1", Value = "V1" }));
                             }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
