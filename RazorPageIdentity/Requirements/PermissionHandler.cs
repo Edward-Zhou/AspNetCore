af42c024dc99023940b098730a0dc66bab8c56ea
Edward <v-tazho@hotmail.com>
2018-11-09 15:10:21 +0800

    sync

--- RazorPageIdentity/Requirements/PermissionHandler.cs
+++ RazorPageIdentity/Requirements/PermissionHandler.cs
@@ -7,54 +7,54 @@ using System.Threading.Tasks;
 
 namespace RazorPageIdentity.Requirements
 {
-    public class PermissionHandler : IAuthorizationHandler
-    {
-        public Task HandleAsync(AuthorizationHandlerContext context)
+        public class PermissionHandler : IAuthorizationHandler
         {
-            var pendingRequirements = context.PendingRequirements.ToList();
+            public Task HandleAsync(AuthorizationHandlerContext context)
+            {
+                var pendingRequirements = context.PendingRequirements.ToList();
 
-            foreach (var requirement in pendingRequirements)
-            {
-                if (requirement is ReadPermission)
+                foreach (var requirement in pendingRequirements)
                 {
-                    if (IsOwner(context.User, context.Resource) ||
-                        IsAdmin(context.User, context.Resource))
+                    if (requirement is ReadPermission)
                     {
-                        context.Succeed(requirement);
+                        if (IsOwner(context.User, context.Resource) ||
+                            IsAdmin(context.User, context.Resource))
+                        {
+                            context.Succeed(requirement);
+                        }
+                    }
+                    else if (requirement is EditPermission ||
+                             requirement is DeletePermission)
+                    {
+                        if (IsOwner(context.User, context.Resource))
+                        {
+                            context.Succeed(requirement);
+                        }
                     }
                 }
-                else if (requirement is EditPermission ||
-                         requirement is DeletePermission)
-                {
-                    if (IsOwner(context.User, context.Resource))
-                    {
-                        context.Succeed(requirement);
-                    }
-                }
+                return Task.CompletedTask;
             }
-            return Task.CompletedTask;
-        }
-        private bool IsAdmin(ClaimsPrincipal user, object resource)
-        {
-            if (user.IsInRole("Admin"))
+            private bool IsAdmin(ClaimsPrincipal user, object resource)
             {
+                if (user.IsInRole("Admin"))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            private bool IsOwner(ClaimsPrincipal user, object resource)
+            {
+                // Code omitted for brevity
+
                 return true;
             }
-            return false;
+
+            private bool IsSponsor(ClaimsPrincipal user, object resource)
+            {
+                // Code omitted for brevity
+
+                return true;
+            }
         }
-
-        private bool IsOwner(ClaimsPrincipal user, object resource)
-        {
-            // Code omitted for brevity
-
-            return true;
-        }
-
-        private bool IsSponsor(ClaimsPrincipal user, object resource)
-        {
-            // Code omitted for brevity
-
-            return true;
-        }
-    }
 }
--- RazorPageIdentity/Requirements/PermissionRequirement.cs
+++ RazorPageIdentity/Requirements/PermissionRequirement.cs
@@ -10,17 +10,17 @@ namespace RazorPageIdentity.Requirements
     {
     }
 
-    public class ReadPermission : IAuthorizationRequirement
-    {
-        // Code omitted for brevity
-    }
-    public class EditPermission : IAuthorizationRequirement
-    {
-        // Code omitted for brevity
-    }
-    public class DeletePermission : IAuthorizationRequirement
-    {
-        // Code omitted for brevity
-    }
+        public class ReadPermission : IAuthorizationRequirement
+        {
+            // Code omitted for brevity
+        }
+        public class EditPermission : IAuthorizationRequirement
+        {
+            // Code omitted for brevity
+        }
+        public class DeletePermission : IAuthorizationRequirement
+        {
+            // Code omitted for brevity
+        }
 
 }
