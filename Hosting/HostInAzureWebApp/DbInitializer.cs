3267a43cbeac291f09b784bf2cd3e713cd40ebf5
Edward <v-tazho@hotmail.com>
2018-11-29 13:57:06 +0800

    host .net core in azure app service

---
+++ Hosting/HostInAzureWebApp/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using HostInAzureWebApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostInAzureWebApp
+{
+    public class DbInitializer
+    {
+
+        public static async Task Initialize(ApplicationDbContext context, UserManager<IdentityUser> userManager,
+                RoleManager<IdentityRole> roleManager, ILogger<DbInitializer> logger)
+        {
+            //context.Database.EnsureCreated();
+
+            // Look for any users.
+            if (context.Users.Any())
+            {
+                return; // DB has been seeded
+            }
+            //await CreateDefaultUserAndRoleForApplication(userManager, roleManager, logger);
+            await CreateDefaultUserTableData(context, userManager, logger);
+        }
+
+        public static async Task CreateDefaultUserTableData(ApplicationDbContext context, UserManager<IdentityUser> userManager,
+            ILogger<DbInitializer> logger)
+        {
+            var user = await userManager.CreateAsync(new IdentityUser { UserName = "Tom", Email = "tom@outlook.com" }
+            , "123@Qwe");
+        }
+
+    }
+}
---
+++ Hosting/HostInAzureWebApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using HostInAzureWebApp.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HostInAzureWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var dbInitializerLogger = services.GetRequiredService<ILogger<DbInitializer>>();
+
+                    context.Database.EnsureCreated();
+                    // DbInitializer.Initialize(context, );
+
+                    DbInitializer.Initialize(context, userManager, roleManager, dbInitializerLogger).Wait();
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
